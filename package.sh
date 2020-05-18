656022e6431a322ee857de2c78c9f77310276754
luowei <luowei@wodedata.com>
2020-05-19 00:46:01 +0800

    修改package.sh

--- package.sh
+++ package.sh
@@ -1,7 +1,9 @@
 # 指定Name
 LibName="LWAspectsHook"
 
+# framework
 pod package ${LibName}.podspec --force --no-mangle --configuration=Release
 
+# .a
 # pod package LWAspectsHook.podspec --force --library --configuration=Release
 
